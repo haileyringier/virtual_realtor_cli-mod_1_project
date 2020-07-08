46ffa204f9989f488ae79a4b109e5d24760b15a6
haileyringier <haileyringier@outlook.com>
2020-07-08 11:02:47 -0600

    updated the global variables to be instance variables

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,15 +1,18 @@
 class Cli
 
-    $name = ""
-    # See if variables can be kept within the method
-
+    @name = ""
+    # See if variables can be kept within the method 
+    @client_bedroom = 0
+    @client_bathroom = 0
+    @client_yard = t
+    @client_location = ""
 
 
     def welcome_user
         puts "Hello there! May I get your name?"
-        $name = gets.chomp
+        @name = gets.chomp
         # Put conditional: Already a client? If not, create.new client
-        puts "Hello #{$name}, Would you like to see a house today? Just say 'yes' or 'no'."
+        puts "Hello #{name}, Would you like to see a house today? Just say 'yes' or 'no'."
         # See if they would either like to see a house or look at previous viewings
         answer = gets.chomp
         # Ask Questions for their house selections. Insert method here
@@ -19,19 +22,19 @@ class Cli
     
     def bedroom_prompt
         puts "How many bedrooms would you like your house to have? Choose between 1-2."
-        $client_bedroom = gets.strip
+        @client_bedroom = gets.strip
     end
     # Allow User to select one of the choices. Make sure there are no duplicates
     
     def bathroom_prompt
         puts "How many bathrooms would you like your house to have? 1-2"
-        $client_bathroom = gets.strip
+        @client_bathroom = gets.strip
     end
     # Allow User to select one of the choices. Make sure there are no duplicates
     
     def backyard_prompt
         puts "Does this dream house need to have a backyard? Just choose 'yes' or 'no'."
-        $client_backyard = gets.strip
+        @client_yard = gets.strip
         # if backyard_choice == "yes"
         #     # Filter selections based on backyard
         # else
@@ -39,10 +42,10 @@ class Cli
         # end
     end
 
-    def city_prompt
+    def location_prompt
         puts "Does this dream house need to have a backyard?
         Just choose between Denver, Littleton, and Boulder."
-        $client_city = gets.strip
+        @client_location = gets.strip
     end
     
     def house_options
@@ -54,8 +57,9 @@ class Cli
     
     # def house_filter
     #     available_houses = House.select do |house|
-    #         House[$client_bedroom] == house["bedrooms"] 
-    #         && House[$client_bathroom] == house["bathrooms"] 
+    #         House[@client_bedroom] == house["bedrooms"] 
+    #       @client_bedroom == house["bedrooms"]
+    #         && House[@client_bathroom] == house["bathrooms"] 
     #         # Insert conditional for boolean backyard
     #     end
     # end
